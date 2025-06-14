354c4c14db817f3a9888e7e305b3bf8a28ff7651
Gabriel Santos <gabrieljorgesilvasantos@outlook.com>
2025-06-14 19:00:57 -0300

    feat: correção

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,7 +1,11 @@
 <template>
     <v-footer color="dark" dark class="py-6" id="footer">
         <v-container class="text-center">
-            &copy; {{ new Date().getFullYear() }} gabrielsantos.tec Todos os direitos reservados.
+            &copy; {{ currentYear }} gabrielsantos.tec Todos os direitos reservados.
         </v-container>
     </v-footer>
 </template>
+
+<script setup>
+const currentYear = new Date().getFullYear()
+</script>
